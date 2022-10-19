3d5ee507f7a1e56893c5ebacd847462a5bd4c3d9
BY-MANISH-KUSHWAHA <manish.kushwaha@blueyonder.com>
2022-10-19 18:18:48 +0530

    add Routes

--- Routes/routes.js
+++ Routes/routes.js
@@ -2,31 +2,32 @@ const express = require("express");
 
 const router = express.Router();
 
+const course_controller = require("../Controller/CourseController")
+
 
 const learner_controller = require("../Controller/LearnerController")
 
-router.post("/add-learner",learner_controller.addLearner)
+router.post("/learners/add-learner",learner_controller.addLearner)
 
-router.get("/",learner_controller.getAllLearner)
+router.get("/learners",learner_controller.getAllLearner)
 
-router.post("/apply-filter",learner_controller.getLearnerByFilter)
+router.post("/learners/apply-filter",learner_controller.getLearnerByFilter)
 
-router.patch("/update",learner_controller.UpdateLearnerByFeilds)
+router.patch("/learners/update",learner_controller.UpdateLearnerByFeilds)
 
-router.delete("/delete",learner_controller.DeleteLearnerByFeilds)
+router.delete("/learners/delete",learner_controller.DeleteLearnerByFeilds)
 
 
-const course_controller = require("../Controller/CourseController")
 
-router.post("/add-course",course_controller.addCourse)
+router.post("/courses/add-course",course_controller.addCourse)
 
-router.get("/",course_controller.getAllCourse)
+router.get("/courses",course_controller.getAllCourse)
 
-router.post("/apply-filter",course_controller.getCourseByFilter)
+router.post("/courses/apply-filter",course_controller.getCourseByFilter)
 
-router.patch("/update",course_controller.UpdateCourseByFeilds)
+router.patch("/courses/update",course_controller.UpdateCourseByFeilds)
 
-router.delete("/delete",course_controller.DeleteCourseByFeilds)
+router.delete("/courses/delete",course_controller.DeleteCourseByFeilds)
 
 
 
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./Routes/routes')
+
+
 var app = express(); 
 
 app.use(express.json());
-
 
 
 
